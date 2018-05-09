2926f4a93ce7d7704ecc7d6b097e2e0fdf6d0829
digitalh2o <digitalh2o@gmail.com>
2018-05-09 12:40:45 -0500

    grabbing sample api data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class AlbumList extends Component {
+  async componentWillMount() {
+    const response = await fetch(
+      "https://rallycoding.herokuapp.com/api/music_albums"
+    );
+    const data = await response.json();
+    console.log(data);
+  }
   render() {
     return (
       <View>
-        <Text>Album List</Text>
+        <Text>Album List!!</Text>
       </View>
     );
   }
