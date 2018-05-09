ff256d9460987477bcbe964178274ae16a4c2271
digitalh2o <digitalh2o@gmail.com>
2018-05-09 13:48:31 -0500

    Separating components to reuse

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import AlbumDetail from "./AlbumDetail";
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   async componentWillMount() {
     const response = await fetch(
       "https://rallycoding.herokuapp.com/api/music_albums"
     );
     const data = await response.json();
-    console.log(data);
+    this.setState({
+      albums: data
+    });
+  }
+
+  renderAlbums() {
+    const { albums } = this.state;
+    return albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
   render() {
-    return (
-      <View>
-        <Text>Album List!!</Text>
-      </View>
-    );
+    console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
