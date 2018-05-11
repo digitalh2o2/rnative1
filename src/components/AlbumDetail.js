80ad3606b988d4678be286616014c77ff790c9d7
digitalh2o <digitalh2o@gmail.com>
2018-05-10 20:30:23 -0500

    styling for app complete. links to external url

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,66 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 
 const AlbumDetail = ({ album }) => {
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: album.thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{album.title}</Text>
+          <Text>{album.artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source={{ uri: album.image }} style={imageStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(album.url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
